88063292d3a4d1d349d8ad1d11c56c3ad8822f86
ashraf032 <101041982+ashraf032@users.noreply.github.com>
2022-03-25 19:01:21 +0400

    result

---
+++ 22.03.19/22.03.19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hesap19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("Bir dord reqemli eded daxil edin");
+                Console.WriteLine("------------------");
+
+                string num = Console.ReadLine();
+
+                int a = int.Parse(num);
+                int b = 7;
+
+                if (a >= 1000 && a <= 10000)
+
+                {
+
+                    Console.WriteLine("Neticeniz");
+
+                    Console.WriteLine("----------");
+
+                    Console.WriteLine($"7{a}8");
+                }
+
+                else
+                {
+                    Console.WriteLine("-----------------------");
+
+
+                    Console.WriteLine("Bu hal nezere alinmayib");
+                }
+            }
+        }
+
+    }
+}
---
+++ 22.03.19/hesap2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hesap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir 3 reqemli eded daxil edin:");
+
+            Console.WriteLine("--------------------");
+
+            string num = Console.ReadLine();
+
+            int a = int.Parse(num);
+
+        }
+    }
+}
